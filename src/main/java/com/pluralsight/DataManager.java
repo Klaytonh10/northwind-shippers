f197c64f86dc0cc4bec29fc769773611bd79cd91
Klaytoh10 <klaytonh10@gmail.com>
2025-12-03 13:36:55 -0600

    set up data manager class, prepared statement, and first requirement of project

--- src/main/java/com/pluralsight/DataManager.java
+++ src/main/java/com/pluralsight/DataManager.java
@@ -1,18 +1,27 @@
 package com.pluralsight;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import com.sun.jdi.connect.spi.Connection;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DataManager {
 
-    public void insertNewShipper(MysqlDataSource dataSource) {
+    public void insertNewShipper(String name, String phone, MysqlDataSource dataSource) {
 
         try(
-                Connection connection = dataSource.getConnection()
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "insert into shippers (CompanyName,Phone) values (?,?)"
+                )
         ) {
-
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phone);
+            int rows = preparedStatement.executeUpdate();
+            try(ResultSet keys = preparedStatement.getGeneratedKeys()) {
+                while(keys.next()) {
+                    System.out.println("Added shipper: " + name + " " + phone);
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,17 +6,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
+    static DataManager dataManager = new DataManager();
 
     public static void main(String[] args) {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL(args[0]);
-        dataSource.setUser(args[1]);
-        dataSource.setPassword(args[2]);
 
-        System.out.println("""
-                
-                """);
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost:3306/northwind");
+        dataSource.setUser("root");
+        dataSource.setPassword("yearup");
+
+        System.out.println("Input the following shipper data:");
+        System.out.print("Shipper Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Shipper phone number: ");
+        String number = scanner.nextLine();
+        dataManager.insertNewShipper(name, number, dataSource);
 
     }
 }
